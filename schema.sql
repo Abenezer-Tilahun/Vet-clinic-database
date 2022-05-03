12c1d310a527443c278945ee26b8228383874022
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-05-03 14:45:38 +0300

    Create the table animals with columns

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+id int GENERATED ALWAYS AS IDENTITY NOT NULL,
+name VARCHAR(30) NOT NULL ,
+date_of_birth date ,
+escape_attempts integer ,
+neutered boolean,
+weight_kg decimal,
+PRIMARY KEY(id)
 );
